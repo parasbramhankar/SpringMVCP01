1a1dec2033327376151ab8e1faf96114fc8f6eb3
parasbramhankar <parasbramhankar.in@gmail.com>
2025-12-26 10:11:47 +0530

    Initial commit

---
+++ src/main/java/com/example/SpringMVCP01/SpringMvcp01Application.java
@@ -0,0 +1,13 @@
+package com.example.SpringMVCP01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcp01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMvcp01Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SpringMVCP01/SpringMvcp01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringMVCP01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvcp01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
